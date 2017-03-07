53f9d28b39191235d544eec180065ebba079a8db
dariukas <dariui.miliauskui@gmail.com>
2017-03-07 20:12:41 +0200

    Initial Commit

---
+++ redditApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  redditApp
+//
+//  Created by Kristina Šlekytė on 07/03/2017.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ redditApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  redditApp
+//
+//  Created by Kristina Šlekytė on 07/03/2017.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
